596e78a3a9d39a34038bd316264e8a7310a2f747
Pia Chouaifaty <piachouaifaty@Pias-MacBook-Pro.local>
2022-05-05 17:23:42 +0300

    calculate pi function

--- lab1_pthreads/pi_serial.c
+++ lab1_pthreads/pi_serial.c
@@ -5,8 +5,26 @@
 #define STEPS 1000000000
 #define STEP_SIZE 1.0/STEPS
 
+double calculate_pi()
+{
+    double lower = 0.5 * STEP_SIZE; 
+    double higher = 1; //1/N, 2/N... N/N--->1
+    double sum = 0;
+
+    //while fraction is less than 1 
+    while(lower < higher)
+    {
+        sum = sum + (STEP_SIZE * sqrt(1 - lower*lower));
+        lower = lower + STEP_SIZE;
+    }
+
+    return sum*4;
+
+}
+
 int main()
 {
+
 
 
 
